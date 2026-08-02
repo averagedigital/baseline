a8e18904d40b18ee8158eb69c150e9533f354025
averagedigital <maintagg@gmail.com>
2026-08-02 14:30:46 +0300

    Добавить детерминированную проверку памяти

--- packages/swift/Package.swift
+++ packages/swift/Package.swift
@@ -7,9 +7,12 @@ let package = Package(
     platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(name: "AthleteCore", targets: ["AthleteCore"]),
+        .library(name: "AthleteAgents", targets: ["AthleteAgents"]),
     ],
     targets: [
         .target(name: "AthleteCore"),
+        .target(name: "AthleteAgents", dependencies: ["AthleteCore"]),
         .testTarget(name: "AthleteCoreTests", dependencies: ["AthleteCore"]),
+        .testTarget(name: "AthleteAgentsTests", dependencies: ["AthleteAgents", "AthleteCore"]),
     ]
 )
